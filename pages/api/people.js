8b7c151b6ad301c03e89a5de2b9a77d7737f2ec6
bat <batysto10@gmail.com>
2022-06-29 14:59:50 +0200

    Simple Generator Pdf w/ NextJS

---
+++ pages/api/people.js
@@ -0,0 +1,35 @@
+
+export default (req, res) => {
+    res.status(200).json([
+      {
+        name: 'Jerome Villaruel',
+        age: '23',
+        address: 'Philippines'
+      },
+      {
+        name: 'Richlyn Hermosilla',
+        age: '18',
+        address: 'Philippines'
+      },
+      {
+        name: 'Lisa Manoban',
+        age: '23',
+        address: 'South Korea'
+      },
+      {
+        name: 'Jennie Kim',
+        age: '24',
+        address: 'South Korea'
+      },
+      {
+        name: 'Roseanne Park',
+        age: '23',
+        address: 'South Korea'
+      },
+      {
+        name: 'Kim Jisoo',
+        age: '25',
+        address: 'South Korea'
+      }
+    ])
+  }
